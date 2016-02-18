f9c451a873475f7c478b01bf005e1123efc9a9a3
spyspyyou - ultrabook <sandro.rueegge@hispeed.ch>
2016-02-18 14:48:22 +0100

    commit

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -1,15 +1,24 @@
 package entities;
 
+import util.PictureImport;
+
+import java.awt.*;
+
 public class Player extends Entity{
 
     private int maxLife;
     private int life;
+    private Image img;
 
     public Player(int xCoordinate, int yCoordinate){
         super(xCoordinate,yCoordinate);
         maxLife = 1000;
         life = maxLife;
+        img= PictureImport.importImage();
     }
 
+    public void render(Graphics g){
+        g.drawImage();
+    }
 
 }
