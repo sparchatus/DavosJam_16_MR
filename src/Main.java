ac8767ca97f9340b0919db488d2aad1ea3891599
Baegi <buks@hispeed.ch>
2016-02-18 14:21:52 +0100

    bla1337

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,37 @@
-import com.sun.deploy.panel.JavaPanel;
+import graphics.GamePanel;
 
 import javax.swing.*;
 
 public class Main extends Thread{
+    public static JFrame frame = new JFrame("MASTERRACE");
+    public static GamePanel panel = new GamePanel();
+
 
     public static void main(String [ ] args){
+        frame.setSize(720, 480);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel.setSize(frame.getSize());
+        frame.add(panel);
+        frame.setVisible(true);
         new Main().start();
     }
 
     public void run(){
-
+        while (true){
+            panel.repaint();
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/input/Keyboard.java
@@ -0,0 +1,25 @@
+package input;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.HashSet;
+
+public class Keyboard implements KeyListener{
+    HashSet<Integer> pressedButtons = new HashSet<Integer>();
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e){
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+}
