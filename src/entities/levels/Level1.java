c8d18214e76818ddd87d85c10cc8a45742fe0dde
spyspyyou - ultrabook <sandro.rueegge@hispeed.ch>
2016-02-19 21:09:52 +0100

    re

--- src/entities/levels/Level1.java
+++ src/entities/levels/Level1.java
@@ -5,6 +5,8 @@ import entities.Level;
 import entities.Player;
 import main.Main;
 
+import java.util.ArrayList;
+
 public class Level1 extends Level {
 
     public Level1(){
@@ -14,6 +16,8 @@ public class Level1 extends Level {
     }
 
     public void enemiesInGame(){
+        Main.activeEnemies = new ArrayList<>();
+        enemiesToSpawn = new ArrayList<>();
         Main.activeEnemies.add(new Enemy(1000, Main.panel.getHeight()-Enemy.SIZE-TILE_SCREEN_SIZE, 1000-Player.WALK_BORDER_RIGHT));
         Main.activeEnemies.add(new Enemy(1500, Main.panel.getHeight()-Enemy.SIZE-TILE_SCREEN_SIZE, 1500-Player.WALK_BORDER_RIGHT));
         enemiesToSpawn.add(new Enemy(2000, Main.panel.getHeight()-Enemy.SIZE-TILE_SCREEN_SIZE, 2000-Player.WALK_BORDER_RIGHT));
--- src/entities/levels/Level2.java
+++ src/entities/levels/Level2.java
@@ -1,9 +1,25 @@
 package entities.levels;
 
+import entities.Enemy;
 import entities.Level;
+import entities.Player;
+import main.Main;
+
+import java.util.ArrayList;
 
 public class Level2 extends Level {
     public Level2() {
         super(3000, "Level_1.png");
+        enemiesInGame();
+    }
+
+    public void enemiesInGame(){
+        Main.activeEnemies = new ArrayList<>();
+        enemiesToSpawn = new ArrayList<>();
+        Main.rainbows = new ArrayList<>();
+        Main.activeEnemies.add(new Enemy(1000, Main.panel.getHeight()-Enemy.SIZE-TILE_SCREEN_SIZE, 1000- Player.WALK_BORDER_RIGHT));
+        Main.activeEnemies.add(new Enemy(1500, Main.panel.getHeight()-Enemy.SIZE-TILE_SCREEN_SIZE, 1500-Player.WALK_BORDER_RIGHT));
+        enemiesToSpawn.add(new Enemy(2000, Main.panel.getHeight()-Enemy.SIZE-TILE_SCREEN_SIZE, 2000-Player.WALK_BORDER_RIGHT));
+        enemiesToSpawn.add(new Enemy(2500, Main.panel.getHeight()-Enemy.SIZE-TILE_SCREEN_SIZE, 2500-Player.WALK_BORDER_RIGHT));
     }
 }
